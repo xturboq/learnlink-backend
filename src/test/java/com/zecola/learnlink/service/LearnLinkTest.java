25a26bb87607d57ce1206de4f9d4776870d82ad1
zecola <xyk816@gmail.com>
2024-06-10 23:21:37 +0800

    测试

--- src/test/java/com/zecola/learnlink/service/LearnLinkTest.java
+++ src/test/java/com/zecola/learnlink/service/LearnLinkTest.java
@@ -3,7 +3,10 @@ package com.zecola.learnlink.service;
 import com.zecola.learnlink.model.domain.User;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
@@ -13,6 +16,8 @@ import java.util.List;
 public class LearnLinkTest {
     @Resource
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Test
     public void testSearchUserByTags() {
@@ -20,4 +25,11 @@ public class LearnLinkTest {
         List<User> userList = userService.searchUsersByTags(tagNameList);
         Assert.assertNotNull(userList);
     }
+    @Test
+    void stringTest(){
+        redisTemplate.opsForValue().set("username","David");
+        String username = (String) redisTemplate.opsForValue().get("username");
+        System.out.println(username);
+    }
+
 }
