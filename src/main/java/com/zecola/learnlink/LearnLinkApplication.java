78b7e403632aa5f4506b7fea44002468b4069d04
zecola <xyk816@gmail.com>
2024-06-14 16:16:30 +0800

    新增 数据导入功能实现

--- src/main/java/com/zecola/learnlink/LearnLinkApplication.java
+++ src/main/java/com/zecola/learnlink/LearnLinkApplication.java
@@ -3,9 +3,11 @@ package com.zecola.learnlink;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.zecola.learnlink.mapper")
+@EnableScheduling
 public class LearnLinkApplication {
 
     public static void main(String[] args) {
