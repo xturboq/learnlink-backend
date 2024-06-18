ef945b31143c59ed399fc0f590bfffad147dfd2e
zecola <xyk816@gmail.com>
2024-06-18 14:37:51 +0800

    新增 Redisson配置

---
+++ src/main/java/com/zecola/learnlink/config/RedissonConfig.java
@@ -0,0 +1,39 @@
+package com.zecola.learnlink.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson 配置
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class RedissonConfig {
+
+    private String host;
+
+    private String port;
+
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        // 1. 创建配置
+        Config config = new Config();
+        String redisAddress = String.format("redis://%s:%s", host, port);
+        // 单个集群模式
+        config.useSingleServer()
+                .setAddress(redisAddress)
+                .setDatabase(2)
+                .setPassword(password);
+        // 2. 创建实例
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
