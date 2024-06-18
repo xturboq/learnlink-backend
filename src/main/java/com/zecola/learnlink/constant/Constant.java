b0b6ab70b97f232f48ea0e99f0c6abc07c0414e6
zecola <xyk816@gmail.com>
2024-06-18 14:59:50 +0800

    新增 常用常量

---
+++ src/main/java/com/zecola/learnlink/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.zecola.learnlink.constant;
+
+public interface Constant {
+    /**
+     * Redisson 锁前缀
+     */
+    String REDISSON_LOCK_PER_KEY = "learnlink:precachejob:docache:lock";
+}
