8512e458400a7c97fe5ad153ef802b74d5659366
zecola <xyk816@gmail.com>
2024-07-02 09:58:16 +0800

    Revert "新增 Redisson连通测试"
    This reverts commit b92beb3aedc55c31d6281c4fa1bb13770c9111b8.

--- src/main/java/com/zecola/learnlink/config/RedisConfig.java
+++ src/main/java/com/zecola/learnlink/config/RedisConfig.java
@@ -19,16 +19,15 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         // 设置连接工厂
         template.setConnectionFactory(connectionFactory);
-//        // 创建JSON序列化工具
-//        GenericJackson2JsonRedisSerializer jsonRedisSerializer =
-//        new GenericJackson2JsonRedisSerializer();
-//        // 设置Key的序列化
-//        template.setKeySerializer(RedisSerializer.string());
-//        template.setHashKeySerializer(RedisSerializer.string());
-//        // 设置Value的序列化
-//        template.setValueSerializer(jsonRedisSerializer);
-//        template.setHashValueSerializer(jsonRedisSerializer);
+        // 创建JSON序列化工具
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer =
+        new GenericJackson2JsonRedisSerializer();
+        // 设置Key的序列化
         template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置Value的序列化
+        template.setValueSerializer(jsonRedisSerializer);
+        template.setHashValueSerializer(jsonRedisSerializer);
         // 返回
         return template;
     }
